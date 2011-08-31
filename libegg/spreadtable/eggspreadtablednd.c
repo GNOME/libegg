5223689cc295f82b30f41b1f96041b7bed1d25d9
Murray Cumming <murrayc@murrayc.com>
2011-08-31 17:06:04 +0200

    EggSpreadTableDnd: Remove unused variable.

--- libegg/spreadtable/eggspreadtablednd.c
+++ libegg/spreadtable/eggspreadtablednd.c
@@ -1019,7 +1019,7 @@ get_index_at_position (EggSpreadTableDnd *spread_table,
 		       gint              *line_ret)
 {
   EggSpreadTable *table;
-  GtkWidget      *widget, *child;
+  GtkWidget      *child;
   GList          *children, *l;
   GtkAllocation   allocation;
   gint            placeholder_cnt = 0;
@@ -1027,7 +1027,6 @@ get_index_at_position (EggSpreadTableDnd *spread_table,
   gint           *segments, lines, line = -1, i, full_size, spacing, position, line_width, first_child;
   gint            index = -1;
 
-  widget = GTK_WIDGET (spread_table);
   table  = EGG_SPREAD_TABLE (spread_table);
 
   /* First find the "line" in question */
